778ce2304065cf5deea2dc5c925ee0f5491977ff
sigran0 <sigran0@gmail.com>
2018-10-19 20:19:53 +0900

    집가자

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 import axios from 'axios'
@@ -8,6 +9,7 @@ import axios from 'axios'
 Vue.config.productionTip = false
 
 Vue.prototype.$http = axios
+Vue.use(Vuex)
 
 /* eslint-disable no-new */
 new Vue({
